39d96463a0356bb14d65ab29d0e51c6b21ce84b1
Sebbon105 <inglevdk@gmail.com>
2025-05-18 19:52:14 +0200

    rock paper scissors is now done after 5 tries

--- script.js
+++ script.js
@@ -1,66 +1,57 @@
-/* Making of the rock paper scissors game */ 
-/* First step is to make a function that randomly returns either rock, paper or scissors, as the computers choice */
+function getComputersChoice(){
+    const random = Math.floor(Math.random() *3) +1;
 
-/* 
-This step is step 4. - this just sets the initial score of the scorecounter
-*/ 
-
-let humanScore = 0;
-let computerScore = 0;
-
-
-function computersChoice() {
-    const random = Math.floor(Math.random() * 3)
-                                                            /* This generates a random number, either 0, 1 or 2 */
-    
-                                                            /* This takes the number generated above and converts its output to a specific string, either: rock paper or scissors */
-    if (random === 0){
+    if (random === 1){
         return "rock"
-    } else if (random === 1) {
+    } else if (random === 2){
         return "paper"
-    } else if (random === 2) {
-        return "scissors"
     } else {
-        return "Answer invalid. Please choose either rock, paper or scissors" 
-    }
-}
-
-console.log(computersChoice());
-
-/*
-1. On start give the user a pop up promt to enter answer
-2. take the answer given
-3. Console.log the answer 
-*/
-
-function humansChoice(a) {
-    const answer = window.prompt("Pick your choice for rock paper scissors")
-    const trimAnswer = answer.trim().toLowerCase()
-
-    return trimAnswer
-}
-
-console.log(humansChoice());
-
-/* 
-Create two variables named humanScore and computerScore in the global scope
-    This is done in the top of the scope.
-*/
-
-function playRound(humansChoice, computersChoice) {
-    if (humansChoice === computersChoice) {
-        console.log(`You both chose ${humansChoice}, its a DRAW`)
-        return;
+        return "scissors"
     }
 
-    
 }
 
-console.log(playRound(humanScore, computerScore));
 
-/* 
-You will write a function that takes the human and computer player choices as arguments
-plays a single round, 
-increments the round winner’s score 
-and logs a winner announcement.
-*/
+function getHumanChoice(){
+    let input = prompt("Type your answer here");
+
+    return input.toLowerCase();
+}
+
+
+function playGame(){
+    let scoreHuman = 0;
+    let scorePc = 0;
+
+    function playRound(){
+    const human = getHumanChoice();
+    const pc = getComputersChoice();
+
+    if (human === pc) {
+        console.log(`Human and pc both chose ${human} - its a DRAW`);
+    } else if (
+        human === "rock" && pc === "scissors" ||
+        human === "scissors" && pc === "paper" ||
+        human === "paper" && pc === "rock"
+    ) {
+        console.log(`Human wins! - ${human} beats ${pc}`);
+        scoreHuman++;
+    } else {
+        console.log(`Pc wins! - ${pc} beats ${human}`)
+        scorePc++;
+    }
+
+    const scoreTable = `The score is now --> Human:${scoreHuman} - PC: ${scorePc}`;
+
+    console.log(scoreTable);
+}
+
+playRound();
+playRound();
+playRound();
+playRound();
+playRound();
+}
+
+playGame();
+
